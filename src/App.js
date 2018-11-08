d688d09911d0e3c0794dde642c372459bce1db16
Shuo <anshuobuaa@gmail.com>
2018-11-08 11:50:28 +0000

    keep the login state and how the log out

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 
 class App extends Component {
 constructor(props) {
   super(props);
 
   this.state = {
-    isAuthenticated: false
+    isAuthenticated: false,
+    isAuthenticating: true
     //first initializes the isAuthenticated flag in the app's flag 
   };
+}
+async componentDidMount() {
+  try {
+    await Auth.currentSession();
+    this.userHasAuthenticated(true);
+  }
+  catch(e) {
+    if (e !== 'No current user') {
+      alert(e);
+    }
+  }
+
+  this.setState({ isAuthenticating: false });
 }
 //this is used to update the flag 
 userHasAuthenticated = authenticated => {
